c6541f37751d275e795127a22b2b6d0400d6c554
Aneesh <aneesh@Aneeshs-MacBook-Air.local>
2025-08-02 18:03:52 +0530

    Responsive fix

--- src/app/layout/HomePage.tsx
+++ src/app/layout/HomePage.tsx
@@ -1,24 +1,25 @@
 import Image from "next/image";
 import { BoxAlign } from "../components/common/BoxAlign";
-import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import {  faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import {
-  faFile,
   faLocation,
-  faMailBulk,
-  faMobile,
   faPhone,
   faEnvelope,
 } from "@fortawesome/free-solid-svg-icons";
 
-const pattern = `bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-black)]/5 max-lg:h-66 max-lg:border-t dark:[--pattern-fg:var(--color-white)]/10`;
-const HomePage = () => {
+const pattern = `
+  bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)]
+  bg-[size:10px_10px]
+  bg-fixed
+  [--pattern-fg:var(--color-black)]/5
+  dark:[--pattern-fg:var(--color-white)]/10
+`;const HomePage = () => {
   return (
     <>
-      <BoxAlign className="w-full max-w-[800px] mx-auto flex justify-between">
+      <BoxAlign className="w-full max-w-[800px] mx-auto flex flex-col md:flex-row items-center md:items-start justify-between">
         <BoxAlign brX className="p-4">
-          <div className="rounded-full p-2  w-[160px] h-[160px] border-[#222] border-[2px]">
+          <div className="rounded-full p-2 w-[160px] h-[160px] border-[#222] border-[2px]">
             <Image
               className="rounded-full"
               src="/photo.jpeg"
@@ -31,7 +32,7 @@ const HomePage = () => {
         <Details />
       </BoxAlign>
       <BoxAlign className={`h-10 ${pattern}`}>
-        <div className="h-full"></div>
+        <div className="h-full w-full" />
       </BoxAlign>
       <BoxAlign
         brX
@@ -67,7 +68,7 @@ const Details = () => {
           <a
             download
             href="/Aneesh_resume_1.pdf"
-            className="bg-[#444] rounded-md border border-[#222] p-2 px-4 flex-grow-0"
+            className="bg-[white] text-[#222] rounded-md border border-[#eee] p-2 px-4 flex-grow-0"
           >
             Download CV
           </a>
@@ -98,9 +99,9 @@ const Self = () => {
   ];
   return (
     <div className="flex flex-col gap-4">
-      {personalDetails.map((detail) => {
+      {personalDetails.map((detail, index) => {
         return (
-          <div className="flex gap-2 items-center" key={detail.text}>
+          <div key={index} className="flex gap-2 items-center">
             <span className="w-8 h-8 p-2 rounded-full bg-[#333] align-middle flex items-center justify-center">
               {detail.icon}
             </span>
